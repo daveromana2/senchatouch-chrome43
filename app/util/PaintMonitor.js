61347a19fcd4b7bb7309c508c1d611a12a63e6e4
stoe <stefan@stoelzle.me>
2015-05-27 09:51:30 +1000

    leave console.info() message during development

--- app/util/PaintMonitor.js
+++ app/util/PaintMonitor.js
@@ -1,16 +1,25 @@
 Ext.define('MyApp.util.PaintMonitor', {
-   override : 'Ext.util.PaintMonitor',
+    override: 'Ext.util.PaintMonitor',
 
-   //Thanks! http://trevorbrindle.com/chrome-43-broke-sencha/
+    uses: [
+        'Ext.env.Browser',
+        'Ext.env.OS',
+        'Ext.util.paintmonitor.CssAnimation',
+        'Ext.util.paintmonitor.OverflowChange'
+    ],
 
-   constructor: function(config) {
-       if (Ext.browser.is.Firefox || (Ext.browser.is.WebKit && Ext.browser.engineVersion.gtEq('536') && !Ext.browser.engineVersion.ltEq('537.36') && !Ext.os.is.Blackberry)) {
-           return new Ext.util.paintmonitor.OverflowChange(config);
-       }
-       else {
-           return new Ext.util.paintmonitor.CssAnimation(config);
-       }
-   }
-}, function(){
-	//console.log("paintmonitor temp. fix is active");
+    //Thanks! http://trevorbrindle.com/chrome-43-broke-sencha/
+
+    constructor: function (config) {
+        if (Ext.browser.is.Firefox || (Ext.browser.is.WebKit && Ext.browser.engineVersion.gtEq('536') && !Ext.browser.engineVersion.ltEq('537.36') && !Ext.os.is.Blackberry)) {
+            return new Ext.util.paintmonitor.OverflowChange(config);
+        }
+        else {
+            return new Ext.util.paintmonitor.CssAnimation(config);
+        }
+    }
+}, function () {
+    // <debug>
+    console.info("Ext.util.PaintMonitor temp. fix is active");
+    // </debug>
 });
