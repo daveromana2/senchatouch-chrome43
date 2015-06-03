03a1c337d9513dd03faa72461113d29f9e379e36
Lee Boonstra <lee.boonstra@sencha.com>
2015-06-03 22:11:26 +0200

    fix ross

--- app/util/PaintMonitor.js
+++ app/util/PaintMonitor.js
@@ -8,16 +8,10 @@ Ext.define('MyApp.util.PaintMonitor', {
         'Ext.util.paintmonitor.OverflowChange'
     ],
 
-    //Thanks! http://trevorbrindle.com/chrome-43-broke-sencha/
-
-    constructor: function (config) {
-        if (Ext.browser.is.Firefox || (Ext.browser.is.WebKit && Ext.browser.engineVersion.gtEq('536') && !Ext.browser.engineVersion.ltEq('537.36') && !Ext.os.is.Blackberry)) {
-            return new Ext.util.paintmonitor.OverflowChange(config);
-        }
-        else {
-            return new Ext.util.paintmonitor.CssAnimation(config);
-        }
+    constructor: function(config) {
+        return new Ext.util.paintmonitor.CssAnimation(config);
     }
+
 }, function () {
     // <debug>
     console.info("Ext.util.PaintMonitor temp. fix is active");
